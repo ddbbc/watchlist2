7146fbf27aa7e53d488ef32914c385faa3e770d1
ddbbc <sd-baby@foxmail.com>
2019-06-17 16:49:33 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -36,6 +36,17 @@ def index():
     return render_template('index.html', user=user, movies=movies)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')
 def initdb(drop):
